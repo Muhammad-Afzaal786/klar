299037e2cc4245c9417ab11a9bce09caa46dca2a
AfzaalKnight786 <afzaalknight786@gmail.com>
2022-03-17 14:27:45 +0500

    Time setting

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -1,27 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import moment from "moment";
 function CurrentTime() {
- let time = new Date();
- let date = new Date();
- const [cTime, setCtime] = useState(time);
- const [cDate, setCdate] = useState(date);
+ const [cTime, setCtime] = useState(moment(new Date()).format("LT"));
 
- const UpdateTime = () => {
-  time = new Date().toLocaleTimeString();
-  setCtime(cTime);
- };
- const UpdateDate = () => {
-  date = new Date().toLocaleDateString();
-  setCdate(cDate);
- };
- setInterval(UpdateDate);
- setInterval(UpdateTime, 500);
+ useEffect(()=>{
+     
+         setInterval(()=>{
+             setCtime(moment(new Date()).format("LT"))
+         },1000)
+     
+     console.log("Hello world")
+ })
  return (
   <div className="mt-3">
-  <div className="time_setting">
-  <h1 className="time_d">{ moment().format("hh:MM")}</h1>
-   <h4 className="data_d">{moment().format("MMM DD, YYYY")}</h4>
-  </div>
+   <div className="time_setting">
+    <h1 className="time_d">{cTime.split(" ")[0]}</h1>
+    <h4 className="data_d">{moment().format("MMM DD, YYYY")}</h4>
+   </div>
   </div>
  );
 }
